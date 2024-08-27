f5e681925a29338c315aa2604ca4de679ae3ff16
Nemanja Simic <nemanjaasimic@gmail.com>
2024-08-27 09:31:54 +0200

    implement pagination, sorting and export to csv for measurements

--- api/measurement_api.py
+++ api/measurement_api.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, Response
+from sqlalchemy import asc, desc
 from model.measurement import Measurement
 from model.location import Location
 from apscheduler.triggers.interval import IntervalTrigger
 from service.collect_measurement import generate_measurement
 from scheduler import scheduler
 from sensor.data_fetcher import fetch_health_status_values
+from datetime import datetime
+from io import StringIO
+import csv
 import uuid
 
 measurement_bp = Blueprint('measurement', __name__, url_prefix='/measurements')
@@ -57,6 +61,11 @@ def stop_scheduler():
 def get_all_measurements():
     query = Measurement.query
 
+    page = request.args.get('page', 1, type=int)
+    size = request.args.get('size', 30, type=int)
+    sort_by = request.args.get('sort_by', 'date')
+    sort_order = request.args.get('sort_order', 'desc')
+
     date_start = request.args.get('date_start')
     date_end = request.args.get('date_end')
     if date_start:
@@ -64,6 +73,8 @@ def get_all_measurements():
             query = query.filter(Measurement.date.between(date_start, date_end))
         else:
             query = query.filter(Measurement.date >= date_start)
+    elif date_end:
+        query = query.filter(Measurement.date <= date_end)
 
     time_start = request.args.get('time_start')
     time_end = request.args.get('time_end')
@@ -72,10 +83,8 @@ def get_all_measurements():
             query = query.filter(Measurement.time.between(time_start, time_end))
         else:
             query = query.filter(Measurement.time >= time_start)
-
-    location_id = request.args.get('location_id')
-    if location_id:
-        query = query.filter_by(location_id=location_id)
+    elif time_end:
+        query = query.filter(Measurement.time <= time_end)
 
     temperature_min = request.args.get('temperature_min')
     temperature_max = request.args.get('temperature_max')
@@ -84,6 +93,8 @@ def get_all_measurements():
             query = query.filter(Measurement.temperature.between(float(temperature_min), float(temperature_max)))
         else:
             query = query.filter(Measurement.temperature >= float(temperature_min))
+    elif temperature_max:
+        query = query.filter(Measurement.temperature <= temperature_max)
 
     relative_humidity_min = request.args.get('relative_humidity_min')
     relative_humidity_max = request.args.get('relative_humidity_max')
@@ -92,6 +103,8 @@ def get_all_measurements():
             query = query.filter(Measurement.relative_humidity.between(float(relative_humidity_min), float(relative_humidity_max)))
         else:
             query = query.filter(Measurement.relative_humidity >= float(relative_humidity_min))
+    elif relative_humidity_max:
+        query = query.filter(Measurement.relative_humidity <= relative_humidity_max)
 
     globe_temperature_min = request.args.get('globe_temperature_min')
     globe_temperature_max = request.args.get('globe_temperature_max')
@@ -100,12 +113,19 @@ def get_all_measurements():
             query = query.filter(Measurement.globe_temperature.between(float(globe_temperature_min), float(globe_temperature_max)))
         else:
             query = query.filter(Measurement.globe_temperature >= float(globe_temperature_min))
+    elif globe_temperature_max:
+        query = query.filter(Measurement.globe_temperature <= globe_temperature_max)
 
     location_id = request.args.get('location_id')
     if location_id:
         query = query.filter_by(location_id=location_id)
 
-    measurements = query.all()
+    if sort_order == 'asc':
+        query = query.order_by(asc(getattr(Measurement, sort_by)))
+    elif sort_order == 'desc':
+        query = query.order_by(desc(getattr(Measurement, sort_by)))
+        
+    measurements = query.paginate(page=page, per_page=size, error_out=False)
 
     measurements_list = [{
         'id': measurement.id,
@@ -124,9 +144,116 @@ def get_all_measurements():
         'uv_b': measurement.uv_b,
         'location_id': measurement.location_id,
         'location_name': measurement.location.name 
-    } for measurement in measurements]
+    } for measurement in measurements.items]
 
-    return jsonify(measurements_list), 200
+    return jsonify({
+        'total': measurements.total,
+        'page': measurements.page,
+        'size': measurements.per_page,
+        'pages': measurements.pages,
+        'has_next': measurements.has_next,
+        'has_prev': measurements.has_prev,
+        'items': measurements_list
+    }), 200
+
+
+@measurement_bp.route("/export", methods=["GET"])
+def get_all_measurements_exported():
+    query = Measurement.query
+
+    sort_by = request.args.get('sort_by', 'date')
+    sort_order = request.args.get('sort_order', 'desc')
+
+    date_start = request.args.get('date_start')
+    date_end = request.args.get('date_end')
+    if date_start:
+        if date_end:
+            query = query.filter(Measurement.date.between(date_start, date_end))
+        else:
+            query = query.filter(Measurement.date >= date_start)
+    elif date_end:
+        query = query.filter(Measurement.date <= date_end)
+
+    time_start = request.args.get('time_start')
+    time_end = request.args.get('time_end')
+    if time_start:
+        if time_end:
+            query = query.filter(Measurement.time.between(time_start, time_end))
+        else:
+            query = query.filter(Measurement.time >= time_start)
+    elif time_end:
+        query = query.filter(Measurement.time <= time_end)
+
+    temperature_min = request.args.get('temperature_min')
+    temperature_max = request.args.get('temperature_max')
+    if temperature_min:
+        if temperature_max:
+            query = query.filter(Measurement.temperature.between(float(temperature_min), float(temperature_max)))
+        else:
+            query = query.filter(Measurement.temperature >= float(temperature_min))
+    elif temperature_max:
+        query = query.filter(Measurement.temperature <= temperature_max)
+
+    relative_humidity_min = request.args.get('relative_humidity_min')
+    relative_humidity_max = request.args.get('relative_humidity_max')
+    if relative_humidity_min:
+        if relative_humidity_max:
+            query = query.filter(Measurement.relative_humidity.between(float(relative_humidity_min), float(relative_humidity_max)))
+        else:
+            query = query.filter(Measurement.relative_humidity >= float(relative_humidity_min))
+    elif relative_humidity_max:
+        query = query.filter(Measurement.relative_humidity <= relative_humidity_max)
+
+    globe_temperature_min = request.args.get('globe_temperature_min')
+    globe_temperature_max = request.args.get('globe_temperature_max')
+    if globe_temperature_min:
+        if globe_temperature_max:
+            query = query.filter(Measurement.globe_temperature.between(float(globe_temperature_min), float(globe_temperature_max)))
+        else:
+            query = query.filter(Measurement.globe_temperature >= float(globe_temperature_min))
+    elif globe_temperature_max:
+        query = query.filter(Measurement.globe_temperature <= globe_temperature_max)
+
+    location_id = request.args.get('location_id')
+    if location_id:
+        query = query.filter_by(location_id=location_id)
+
+
+    if sort_order == 'asc':
+        query = query.order_by(asc(getattr(Measurement, sort_by)))
+    elif sort_order == 'desc':
+        query = query.order_by(desc(getattr(Measurement, sort_by)))
+    
+    measurements = query.all()
+
+    si = StringIO()
+    writer = csv.writer(si)
+    writer.writerow(['Location', 'Date', 'Time', 'Altitude (m)', 'Longitude', 'Latitude', 't (°C)', 'RH (%)', 'Tg(°C)', 'v (m/s)', 'v [0.5-17] (m/s)', 'PM (2.5) ppm', 'PM (10) ppm', 'UV B (mW/cm^2)'])
+    for m in measurements:
+        writer.writerow([
+            m.location.name if m.location else '',
+            m.date.strftime('%Y-%m-%d'),
+            m.time.strftime('%H:%M:%S'),
+            m.altitude,
+            m.longitude,
+            m.latitude,
+            m.temperature,
+            m.relative_humidity,
+            m.globe_temperature,
+            m.wind_speed,
+            m.limited_wind_speed,
+            m.pm_2_5,
+            m.pm_10,
+            m.uv_b
+        ])
+
+    si.seek(0)
+
+    today_date = datetime.today().strftime('%Y-%m-%d')
+    output = Response(si.getvalue(), mimetype='text/csv')
+    output.headers['Content-Disposition'] = f'attachment; filename=measurements_{today_date}.csv'
+    output.status_code = 200
+    return output
 
 @measurement_bp.route("/status", methods=["GET"])
 def get_sensor_status():
